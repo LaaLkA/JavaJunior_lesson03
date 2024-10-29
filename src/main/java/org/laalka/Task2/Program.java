7fcfa67b1e8c20cc509bc20cbe4587a3f8dca31a
LaaLkA <n.afonchenkov@yandex.ru>
2024-10-29 11:36:57 +0300

    Sem completed

--- src/main/java/org/laalka/Task2/Program.java
+++ src/main/java/org/laalka/Task2/Program.java
@@ -1,6 +1,61 @@
 package org.laalka.Task2;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.laalka.Task2.ToDoListApp.*;
+
 public class Program {
 
+    public static void main(String[] args) {
+        List<ToDo> tasks;
+        File f = new File(FILE_JSON);
+        if (f.exists() && !f.isDirectory()) {
+            tasks = loadTasksFromFile(FILE_JSON);
+        } else {
+            tasks = prepareTasks();
+        }
+        ToDoListApp.saveTasksToFile(FILE_JSON, tasks);
+        ToDoListApp.saveTasksToFile(FILE_BIN, tasks);
+        ToDoListApp.saveTasksToFile(FILE_XML, tasks);
 
+        displayTasks(tasks);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Выберите действие:\n" +
+                    "1. Добавить новую задачу\n" +
+                    "2. Отметить задачу как выполненую\n" +
+                    "3. Выйти");
+
+            String action = scanner.nextLine();
+            switch (action) {
+                case "1":
+                    ToDoListApp.addNewTask(scanner, tasks);
+                case "2":
+                    ToDoListApp.markTaskAsDone(scanner, tasks);
+                    break;
+                case "3":
+                    ToDoListApp.saveTasksToFile(FILE_JSON, tasks);
+                    ToDoListApp.saveTasksToFile(FILE_BIN, tasks);
+                    ToDoListApp.saveTasksToFile(FILE_XML, tasks);
+                    System.out.println("Список задач сохранён.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Некорректный выбор. Попробуйте снова");
+            }
+            displayTasks(tasks);
+        }
+    }
+
+    static List<ToDo> prepareTasks() {
+        ArrayList<ToDo> list = new ArrayList<>();
+        list.add(new ToDo("Сходить в магазин за продуктами"));
+        list.add(new ToDo("Полгулять с собакой"));
+        list.add(new ToDo("Заменить лампочку"));
+        return list;
+    }
 }
--- src/main/java/org/laalka/Task2/ToDoV1.java
+++ src/main/java/org/laalka/Task2/ToDoV1.java
@@ -7,7 +7,47 @@ public class ToDoV1 implements Serializable {
     //region Поля
     /**
      * Наименование задачи
-     * 46:32
-     * */
+     */
+    private String title;
+
+    /**
+     * Статус задачи
+     */
+    private boolean isDone;
+    //endregion
+
+    //region Конструкторы
+
+    public ToDoV1(){
+
+    }
+
+    public ToDoV1(String title, boolean isDone) {
+        this.title = title;
+        this.isDone = isDone;
+    }
+
+    //endregion
+
+    //region Методы
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    //endregion
+
 
 }
